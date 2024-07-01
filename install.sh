d1e2676256d2301aec211bcd868bb66a8146a22b
smruti-s <suresh.sm@northeastern.edu>
2024-07-01 16:07:10 -0400

    Bash script for installing ros2 and other basic packages

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update && sudo apt-get upgrade
+sudo apt install terminator git python3-pip -y 
+sudo apt-get install build-essential -y 
+pip install jupyterlab
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale #verify settings
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt upgrade
+sudo apt install ros-humble-desktop
+
